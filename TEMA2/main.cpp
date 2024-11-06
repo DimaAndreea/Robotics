b1796ce2b0a81acd5dbc82f7705882260d8fe82d
DimaAndreea <149427546+DimaAndreea@users.noreply.github.com>
2024-11-06 13:29:59 +0200

    Update main.cpp

--- TEMA2/main.cpp
+++ TEMA2/main.cpp
@@ -4,36 +4,39 @@
 const int ledRGB_R = 6;
 const int ledRGB_G = 5;
 const int ledRGB_B = 4;
-const int startBtn = 3;       // buton pentru pornire/oprie joc
-const int difficultyBtn = 2;   // buton pentru schimbarea dificultatii
-
-// timpul minim dintre doua apasari pentru a preveni apasarile accidentale (debouncing)
-const unsigned long debounceDelay = 200;
-
-// variabile pentru a retine starea butoanelor
-unsigned long lastDifficultyPressTime = 0;
-unsigned long lastStartStopPressTime = 0;
-bool startBtnLastState = HIGH;
-bool difficultyBtnLastState = HIGH;
+const int startBtn = 3;         // buton pentru pornire/oprie joc
+const int difficultyBtn = 2;        // buton pentru schimbarea dificultatii
 
 // configuram timpul total al rundei si durata animatiei de start
 const unsigned long roundTime = 30000; // 30 secunde
 
 // timpi pentru inceputul rundei si pentru afisarea cuvintelor
-unsigned long roundStart = 0;
-unsigned long wordStart = 0;
+unsigned long roundStart = 0;   // momentul in care incepe o runda
+unsigned long wordDisplay = 0;     // momentul in care un cuvant nou este afisat
 
-String currentWord = "";
-String userInput = "";  // inputul introdus de utilizator
+// variabile pentru a retine starea butoanelor
+unsigned long lastStartPress = 0;       // timpul la care butonul de start/stop a fost apasat
+unsigned long lastDiffPress = 0;        // timpul la care butonul de dificultate a fost apasat
+bool startBtnState = HIGH;      // ultima stare a butonului de strt/stop
+bool difficultyBtnState = HIGH;         // ultima stare a butonului de dificultate 
+
+// timpul minim dintre doua apasari pentru a preveni apasarile accidentale (debouncing)
+const unsigned long debounceDelay = 200;
+
+String currentWord = "";  // cuvantul curent ce trebuie tastat
+String userInput = "";    // inputul introdus de utilizator
+
+unsigned long timePerWord = 0;  // timpul alocat pentru fiecare cuvant
+int correctWordsCounter = 0;    // contor pentru cuvintele tastate corect
 
 // variabile volatile pentru a gestiona schimbarile de stare ale jocului
 volatile bool shouldCycleDifficulty = false;
 volatile bool shouldStartGame = false;
 volatile bool shouldStopGame = false;
-volatile bool gameStarted = false;
+volatile bool isGameStarted = false;
 volatile bool shouldChangeWord = false;
 
-// lista de cuvinte pentru joc
+// dictionarul de cuvinte pentru joc
 const String words[50] = {
   "spark", "blissful", "wander", "eclipse", "luminous", "fragment", "radiant", "echoes",
   "cascade", "voyage", "horizon", "illusion", "whisper", "enchant", "meander", "quaint",
@@ -45,23 +48,23 @@ const String words[50] = {
 };
 
 // enum pentru dificultate
-enum gameMode {
+enum Difficulty {
   UNKNOWN = -1,
   EASY = 0,
   MEDIUM = 1,
   HARD = 2
 };
 
-gameMode difficulty = gameMode::UNKNOWN;
-unsigned long timePerWord = 0;
-int correctWords = 0;
+Difficulty difficulty = Difficulty::UNKNOWN;        //setam dificulatea la unknown 
 
 // functie pentru a schimba dificultatea
-void cycleGameMode() {
+void cycleDifficulty() {
     shouldCycleDifficulty = false;
 
-    difficulty = (gameMode)(((int)difficulty + 1) % 3); // schimbam prin cele trei nivele: usor, mediu, greu
+    // rotim dificultatea intre EASY, MEDIUM si HARD
+    difficulty = (Difficulty)(((int)difficulty + 1) % 3);
 
+    // setam timpul per cuvant in functie de dificultate
     switch (difficulty) {
         case EASY:
             Serial.println("Easy mode on!");
@@ -85,16 +88,19 @@ void cycleGameMode() {
 
 // functie pentru a seta starea inactiva (joc oprit)
 void idleState() {
-    digitalWrite(ledRGB_R, HIGH);  
-    digitalWrite(ledRGB_G, HIGH); 
-    digitalWrite(ledRGB_B, HIGH); 
+    // resetam LED-ul RGB la culoarea alba
+    digitalWrite(ledRGB_R, HIGH);
+    digitalWrite(ledRGB_G, HIGH);
+    digitalWrite(ledRGB_B, HIGH);
+    
+    // resetam variabilele jocului
     difficulty = UNKNOWN;
     timePerWord = 0;
     shouldCycleDifficulty = false;
     shouldStartGame = false;
     shouldStopGame = false;
-    gameStarted = false;
-    correctWords = 0;
+    isGameStarted = false;
+    correctWordsCounter = 0;
     shouldChangeWord = false;
     currentWord = "";
     userInput = "";
@@ -107,53 +113,58 @@ void startGame() {
     shouldStartGame = false;
     shouldStopGame = false;
 
+    // verificam daca dificultatea este setata
     if (difficulty == UNKNOWN) {
         Serial.println("Please choose difficulty.");
         return;
     }
 
-    gameStarted = true;
-    correctWords = 0;
+    isGameStarted = true;
+    correctWordsCounter = 0;
     userInput = "";  // resetam inputul utilizatorului
 
+    // animatie de numaratoare inversa de 3 secunde
     for (int i = 3; i > 0; i--) {
         Serial.println(i);
-        digitalWrite(ledRGB_R, HIGH);  
-        digitalWrite(ledRGB_G, HIGH); 
-        digitalWrite(ledRGB_B, HIGH); 
-        delay(500);
-        digitalWrite(ledRGB_R, LOW);  
-        digitalWrite(ledRGB_G, LOW); 
-        digitalWrite(ledRGB_B, LOW); 
-        delay(500);
+        digitalWrite(ledRGB_R, HIGH);
+        digitalWrite(ledRGB_G, HIGH);
+        digitalWrite(ledRGB_B, HIGH);
+        delay(500);  // delay de 500 ms
+        digitalWrite(ledRGB_R, LOW);
+        digitalWrite(ledRGB_G, LOW);
+        digitalWrite(ledRGB_B, LOW);
+        delay(500);  // delay de 500 ms
     }
 
-    digitalWrite(ledRGB_R, LOW);  
-    digitalWrite(ledRGB_G, HIGH); 
-    digitalWrite(ledRGB_B, LOW); 
+    // aprindem LED-ul in verde pentru a indica ca jocul a pornit
+    digitalWrite(ledRGB_R, LOW);
+    digitalWrite(ledRGB_G, HIGH);
+    digitalWrite(ledRGB_B, LOW);
 
-    roundStart = millis();
-    shouldChangeWord = true;
-}
-
-// functie pentru a opri jocul
-void stopGame() {
-    Serial.print("Score: ");
-    Serial.println(correctWords);
-
-    idleState();
+    roundStart = millis();  // salvam timpul de start al rundei
+    shouldChangeWord = true;                // semnalam ca trebuie sa schimbam cuvantul
 }
 
 // functie pentru a schimba cuvantul curent
 void changeWord() {
     shouldChangeWord = false;
 
+    // selectam un cuvant aleatoriu din lista
     int index = random(0, 50);
     currentWord = words[index];
     userInput = "";  // resetam inputul utilizatorului pentru noul cuvant
 
-    wordStart = millis();  // salvam momentul afisarii noului cuvant
+    wordDisplay = millis();  // salvam timpul afisarii noului cuvant
     Serial.println("\nNew word: " + currentWord);  // afisam noul cuvant
+}
+
+// functie pentru a opri jocul
+void stopGame() {
+    // afisam scorul final
+    Serial.print("Score: ");
+    Serial.println(correctWordsCounter);
+
+    idleState();  // punem jocul in stare inactiva
 }
 
 void setup() {
@@ -165,13 +176,15 @@ void setup() {
     pinMode(startBtn, INPUT_PULLUP); 
     pinMode(difficultyBtn, INPUT_PULLUP);  
 
-    digitalWrite(ledRGB_R, HIGH);  
-    digitalWrite(ledRGB_G, HIGH); 
-    digitalWrite(ledRGB_B, HIGH);  
+    // initializam LED-ul RGB in alb
+    digitalWrite(ledRGB_R, HIGH);
+    digitalWrite(ledRGB_G, HIGH);
+    digitalWrite(ledRGB_B, HIGH);
 
-    randomSeed(analogRead(A0));  // initializam generatorul de numere aleatorii pentru a nu avea aceeasi ordine
+    // initializam generatorul de numere aleatorii pentru a nu avea aceeasi ordine
+    randomSeed(analogRead(A0));
 
-    idleState();
+    idleState();  // setam jocul in stare inactiva la inceput
 }
 
 void loop() {
@@ -179,48 +192,49 @@ void loop() {
 
     // verificam butonul de dificultate pentru debounce
     bool difficultyBtnState = digitalRead(difficultyBtn);
-    if (difficultyBtnState == LOW && difficultyBtnLastState == HIGH && currentMillis - lastDifficultyPressTime > debounceDelay) {
-        lastDifficultyPressTime = currentMillis;
-        cycleGameMode();
+    if (difficultyBtnState == LOW && difficultyBtnState == HIGH && currentMillis - lastDiffPress > debounceDelay) {
+        lastDiffPress = currentMillis;
+        cycleDifficulty();  // schimbam dificultatea
     }
-    difficultyBtnLastState = difficultyBtnState;
+    difficultyBtnState = difficultyBtnState;
 
     // verificam butonul de start/stop pentru debounce
     bool startBtnState = digitalRead(startBtn);
-    if (startBtnState == LOW && startBtnLastState == HIGH && currentMillis - lastStartStopPressTime > debounceDelay) {
-        lastStartStopPressTime = currentMillis;
-        if (gameStarted) {
-            stopGame();
+    if (startBtnState == LOW && startBtnState == HIGH && currentMillis - lastStartPress > debounceDelay) {
+        lastStartPress = currentMillis;
+        if (isGameStarted) {
+            stopGame();  // oprim jocul
         } else {
-            startGame();
+            startGame();  // pornim jocul
         }
     }
-    startBtnLastState = startBtnState;
+    startBtnState = startBtnState;
 
     // logica jocului cand este pornit
-    if (gameStarted) {
-        // verificam daca timpul pentru cuvantul curent a expirat si schimbam cuvantul
-        if (millis() - wordStart >= timePerWord) {
-            shouldChangeWord = true;
+    if (isGameStarted) {
+        // verificam daca timpul pentru cuvantul curent a expirat
+        if (millis() - wordDisplay >= timePerWord) {
+            shouldChangeWord = true;  // semnalam schimbarea cuvantului
         }
 
         if (shouldChangeWord) {
-            changeWord();
+            changeWord();  // schimbam cuvantul curent
         }
 
-        // citire si verificare continua a cuvantului in timp real
+        // citim inputul utilizatorului
         if (Serial.available()) {
             char inputChar = Serial.read();  // citim caracterul introdus
             
             // afisam caracterul tastat in timp real
             if (inputChar != '\b') {
-                Serial.print(inputChar);  // afisam caracterul tastat daca nu este Backspace
+                Serial.print(inputChar);  // afisam caracterul daca nu este Backspace
             }
 
-            if (inputChar == '\b') {  // verificam daca este Backspace
+            // gestionam stergerea caracterului cu Backspace
+            if (inputChar == '\b') {
                 if (userInput.length() > 0) {
                     userInput.remove(userInput.length() - 1);  // stergem ultima litera
-                    Serial.print("\b \b"); // sterge vizual ultima litera din Serial Monitor
+                    Serial.print("\b \b"); // stergem ultima litera din Serial Monitor
                 }
             } else if (isPrintable(inputChar)) {
                 userInput += inputChar;  // adaugam caracterul la input
@@ -228,26 +242,28 @@ void loop() {
 
             // verificam daca inputul este gresit
             if (userInput != currentWord.substring(0, userInput.length())) {
-                digitalWrite(ledRGB_R, HIGH);  
-                digitalWrite(ledRGB_G, LOW); 
-                digitalWrite(ledRGB_B, LOW); 
+                // aprindem LED-ul in rosu pentru input gresit
+                digitalWrite(ledRGB_R, HIGH);
+                digitalWrite(ledRGB_G, LOW);
+                digitalWrite(ledRGB_B, LOW);
             } else {
-                digitalWrite(ledRGB_R, LOW);  
-                digitalWrite(ledRGB_G, HIGH); 
-                digitalWrite(ledRGB_B, LOW); 
+                // aprindem LED-ul in verde pentru input corect
+                digitalWrite(ledRGB_R, LOW);
+                digitalWrite(ledRGB_G, HIGH);
+                digitalWrite(ledRGB_B, LOW);
             }
 
-            // daca utilizatorul a introdus cuvantul corect, generam un cuvant nou si resetam timpul
+            // daca utilizatorul a introdus cuvantul corect
             if (userInput == currentWord) {
-                correctWords++;
-                shouldChangeWord = true;
-                wordStart = millis();  // resetam temporizatorul pentru noul cuvant
+                correctWordsCounter++;  // incrementam contorul
+                shouldChangeWord = true;  // semnalam schimbarea cuvantului
+                wordDisplay = millis();  // resetam timpul pentru noul cuvant
             }
         }
 
         // verificam daca timpul pentru joc s-a terminat
         if (millis() - roundStart >= roundTime) {
-            stopGame();
+            stopGame();  // oprim jocul
         }
     }
 }
